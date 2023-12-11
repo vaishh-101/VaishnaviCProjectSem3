d97f3701fb5b836791d581e04d56e4e082dba0de
vaishh-101 <vaishnavichoudhary200@gmail.com>
2023-12-11 21:01:02 +0530

    new commit

--- server/index.js
+++ server/index.js
@@ -49,10 +49,10 @@ app.get("/", function(req, res) {
 });
 
 if(process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, '..', 'frontend', 'build')));
+  app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 
   app.get('*', (req, res) => {
-     res.sendFile(path.join(__dirname, '..', 'frontend', 'build', 'index.html'))
+     res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'))
  });
 }
 
